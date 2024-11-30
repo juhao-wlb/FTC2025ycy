d02dc1e26580503704c3492b03ca2eb468920e23
Chao-Freeman <124365937+Chao-Freeman@users.noreply.github.com>
2024-11-30 18:06:48 +0800

    Alphabot: Auto: Part of Robot PID

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/drivetrain/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/drivetrain/SampleMecanumDrive.java
@@ -56,8 +56,8 @@ import lombok.Getter;
  */
 @Config
 public class SampleMecanumDrive extends MecanumDrive {
-    public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(0, 0, 0);
-    public static PIDCoefficients HEADING_PID = new PIDCoefficients(0, 0, 0);
+    public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(1, 0, 0);
+    public static PIDCoefficients HEADING_PID = new PIDCoefficients(5, 0, 0.2);
 
     public static double LATERAL_MULTIPLIER = 1.4514;//;167.64/115.5;
 
